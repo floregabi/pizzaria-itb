b7fe83db56f6a2cdb5216b4d97b3ae2691becdfe
Gabriela de Araujo Florentino <rm98926@estudante.fieb.edu.br>
2026-05-28 09:15:54 -0300

    Menu e Home do funcionários adicionados

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import './App.css'
+import HomeFuncionario from './pages/HomeFuncionario/HomeFuncionario'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-     
+    <div>
+      <HomeFuncionario/>
+    </div>
     </>
   )
 }
