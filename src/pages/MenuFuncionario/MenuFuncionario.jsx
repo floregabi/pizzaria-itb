b3d94c521081fc90fff7db796442b033b2d588cc
Gabriela de Araujo Florentino <rm98926@estudante.fieb.edu.br>
2026-09-21 07:41:15 -0300

    Menu e Home do Funcionário em Construção

---
+++ src/pages/MenuFuncionario/MenuFuncionario.jsx
@@ -0,0 +1,108 @@
+
+import styles from "./MenuFuncionario.module.css"
+
+import { Link } from "react-router-dom"
+
+
+const MenuFuncionario = () => {
+
+    return (
+        <div>
+       <nav className={`navbar navbar-expand-lg navbar-light bg-light p-2 rounded shadow-sm w-100 ${styles.menu}`}>
+        
+        <Link 
+          to="/home"
+          className={`navbar-brand ${styles.logo}`}
+        >
+          Home
+        </Link>
+
+        {/* Botão Hamburguer para telas menores */}
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto">
+            <li className="nav-item active">
+              <Link
+              to="/produtos"
+              className={`nav-link ${styles.itemMenu}`}
+              >
+                Produtos
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+              to="/categoria"
+              className={`nav-link ${styles.itemMenu}`}
+              >
+                Categorias
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+              to="/estoque"
+              className={`nav-link ${styles.itemMenu}`}
+              >
+                Estoque
+              </Link>
+            </li>
+
+            {/* Dropdown Menu */}
+            <li className="nav-item dropdown">
+              <a
+                className="nav-link dropdown-toggle"
+                href="#"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                Opções
+              </a>
+              <ul className="dropdown-menu">
+                <li>
+                  <a className="dropdown-item" href="#">
+                    Ação 1
+                  </a>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="#">
+                    Ação 2
+                  </a>
+                </li>
+                <li>
+                  <hr className="dropdown-divider" />
+                </li>
+                <li>
+                  <a className="dropdown-item" href="#">
+                    Outra opção
+                  </a>
+                </li>
+              </ul>
+            </li>
+
+            <li className="nav-item">
+              <a className="nav-link disabled">Desativado</a>
+            </li>
+          </ul>
+
+          {/* Botão de Logout alinhado à direita */}
+          <button type="button" className="btn btn-primary">
+            Logout
+          </button>
+        </div>
+      </nav>
+        </div>
+    )
+}
+
+export default MenuFuncionario
